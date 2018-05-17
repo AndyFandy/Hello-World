40418e76c9f9348de0fc81dce6e8279821c1284e
Andy <andrewmcneil870@yahoo.com>
2018-05-16 23:38:42 -0600

    Learning addition

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+num1 = input('enter number.\n')
+if num1.isnumeric() !=True:
+    num1 = input('seriously, enter a number, fuckhead.\n')
+num2 = input('enter second number.\n')
+if num2.isnumeric() !=True:
+    num2 = input('for reals this time.\n')
+print(float(num1) + float(num2))
+
+
+
